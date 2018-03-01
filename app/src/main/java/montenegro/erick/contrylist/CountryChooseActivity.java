9a661c8c80d74071280c4108ae07ef3f213c398a
Erick <erickma.1234@gmail.com>
2018-03-01 09:21:32 +0100

    Seleccionamos el país y nos lo muestra en la página principal

--- app/src/main/java/montenegro/erick/contrylist/CountryChooseActivity.java
+++ app/src/main/java/montenegro/erick/contrylist/CountryChooseActivity.java
@@ -1,7 +1,10 @@
 package montenegro.erick.contrylist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -13,7 +16,7 @@ public class CountryChooseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_country_choose);
 
         ListView list = (ListView) findViewById(R.id.list);
-        String[] countries = getResources().getStringArray(R.array.countries);
+        final String[] countries = getResources().getStringArray(R.array.countries); //final perq sino el adapterview no pot accedir-hi
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_list_item_1,
@@ -21,5 +24,14 @@ public class CountryChooseActivity extends AppCompatActivity {
         );
 
         list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent data = new Intent();
+                data.putExtra("country", countries[position]);
+                setResult(RESULT_OK, data);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/montenegro/erick/contrylist/FormActivity.java
+++ app/src/main/java/montenegro/erick/contrylist/FormActivity.java
@@ -1,17 +1,36 @@
 package montenegro.erick.contrylist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class FormActivity extends AppCompatActivity {
+
+    private static final int CHOOSE_COUNTRY = 1;
+    private TextView country;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        country = (TextView) findViewById(R.id.country);
     }
 
     public void onChooseCountry(View view) {
+        Intent intent = new Intent(this, CountryChooseActivity.class);
+        startActivityForResult(intent, CHOOSE_COUNTRY); //REFERENCIAMOS a que intent nos referimos, y guardamos el entero que nos dice qúe devuelve. en este caso solo hay 1
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case CHOOSE_COUNTRY: //nomes tenim 1 case
+                if (resultCode==RESULT_OK){
+                    country.setText(data.getStringExtra("country"));
+                }
+        }
     }
 }
