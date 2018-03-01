9a52aa3b7b3e844fa65180e0b7eb2e6b836ebbb8
Erick <erickma.1234@gmail.com>
2018-03-01 09:00:37 +0100

    App per seleccionar un país d'una llista amb dos activitats: form activity, CountryChooseActvity

---
+++ app/src/main/java/montenegro/erick/contrylist/CountryChooseActivity.java
@@ -0,0 +1,25 @@
+package montenegro.erick.contrylist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class CountryChooseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country_choose);
+
+        ListView list = (ListView) findViewById(R.id.list);
+        String[] countries = getResources().getStringArray(R.array.countries);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                countries
+        );
+
+        list.setAdapter(adapter);
+    }
+}
